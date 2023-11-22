4af0d04e60e68187f9bc2713a8368d345fb3150b
Gustavo Duarte <gutorap.gh@gmail.com>
2023-11-22 10:18:02 -0300

    substitute normalize.css for tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['normalize.css/normalize.css'],
+  modules: ['@nuxtjs/tailwindcss'],
 })
